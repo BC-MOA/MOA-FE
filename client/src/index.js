f9d251eab141a64021ed3ff19fd2456fb5ec7dc2
sieun <sieun0115@ajou.ac.kr>
2022-01-19 18:42:52 +0900

    feat: 전역으로 색상변수 등록

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "../src/style/fonts.css";
+import colors from "./style/colors";
+import { createGlobalStyle } from "styled-components";
 import reportWebVitals from "./reportWebVitals";
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    ${colors}
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
