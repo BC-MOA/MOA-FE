8ba1a4dc3199c03b9e497ae22c3724dfc51c821a
sieun <sieun0115@ajou.ac.kr>
2022-01-19 20:42:48 +0900

    feat: 색상 js 파일을 css 파일로 변경

--- client/src/index.js
+++ client/src/index.js
@@ -3,19 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "../src/style/fonts.css";
-import colors from "./style/colors";
-import { createGlobalStyle } from "styled-components";
+import "./style/color.css";
 import reportWebVitals from "./reportWebVitals";
-
-const GlobalStyle = createGlobalStyle`
-  :root {
-    ${colors}
-  }
-`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
