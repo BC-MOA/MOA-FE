c5c435ff49b76664c7914a9100b7755e8d26a160
0andme <yu00mi97@gmail.com>
2022-01-21 00:55:33 +0900

    #5 feat : get 가입가능적금리스트 api 호출함수 생성 및 금리순 정렬기능 추가
    가입가능 적금 리스트 수정
    - api 호출 함수까지 제작
    최고금리순 / 기본금리순 정렬 기능 추가

--- client/src/components/gather/AvailableSavingList.js
+++ client/src/components/gather/AvailableSavingList.js
@@ -1,69 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AvailableSavingItem from "./AvailableSavingItem";
 import { v1 as uuid } from "uuid";
 import styled from "styled-components";
+import { useMemo } from "react/cjs/react.development";
 
-function AvailableSavingList() {
+function AvailableSavingList({ listControl }) {
+  const [availableSavingList, setAvailableSavingList] = useState([]);
+  const sortedSavingList = useMemo(() => {
+    const copyList = JSON.parse(JSON.stringify(availableSavingList));
+    if ("최고금리순" === listControl) {
+      return copyList.sort((a, b) => {
+        return b.최대금리 - a.최대금리;
+      });
+    } else {
+      return copyList.sort((a, b) => {
+        return b.기본금리 - a.기본금리;
+      });
+    }
+  }, [listControl, availableSavingList]);
+  useEffect(() => {
+    getSavingList();
+  }, []);
   // todo - api datas
-  const availableSavingList = [
-    {
-      은행명: "신한",
-      은행코드: "088",
-      적금명: "신한 장병내일준비적금",
-      기본금리: 4.5,
-      최대금리: 5.2,
-      최소기간: 6,
-      최대기간: 12,
-    },
-    {
-      은행명: "제주은행",
-      은행코드: "035",
-      적금명: "제주은행 장병내일준비적금",
-      기본금리: 4.5,
-      최대금리: 5.2,
-      최소기간: 6,
-      최대기간: 12,
-    },
-    {
-      은행명: "IBK",
-      적금명: "IBK 장병내일준비적금",
-      은행코드: "225",
-      기본금리: 4.5,
-      최대금리: 5.2,
-      최소기간: 6,
-      최대기간: 12,
-    },
-    {
-      은행명: "우체국",
-      은행코드: "071",
-      적금명: "우체국 장병내일준비적금",
-      기본금리: 4.5,
-      최대금리: 5.2,
-      최소기간: 6,
-      최대기간: 12,
-    },
-    {
-      은행명: "하나",
-      은행코드: "081",
-      적금명: "하나 장병내일준비적금",
-      기본금리: 4.0,
-      최대금리: 5.0,
-      최소기간: 6,
-      최대기간: 12,
-    },
-    {
-      은행명: "NH",
-      은행코드: "011",
-      적금명: "NH 장병내일준비적금",
-      기본금리: 4.0,
-      최대금리: 5.0,
-      최소기간: 6,
-      최대기간: 12,
-    },
-  ];
+  function getSavingList() {
+    setAvailableSavingList([
+      {
+        은행명: "신한",
+        은행코드: "088",
+        적금명: "신한 장병내일준비적금",
+        기본금리: 4.5,
+        최대금리: 5.7,
+        최소기간: 6,
+        최대기간: 12,
+      },
+      {
+        은행명: "제주은행",
+        은행코드: "035",
+        적금명: "제주은행 장병내일준비적금",
+        기본금리: 4.5,
+        최대금리: 5,
+        최소기간: 6,
+        최대기간: 12,
+      },
+      {
+        은행명: "IBK",
+        적금명: "IBK 장병내일준비적금",
+        은행코드: "225",
+        기본금리: 4.5,
+        최대금리: 5.5,
+        최소기간: 6,
+        최대기간: 12,
+      },
+      {
+        은행명: "우체국",
+        은행코드: "071",
+        적금명: "우체국 장병내일준비적금",
+        기본금리: 4.5,
+        최대금리: 5.5,
+        최소기간: 6,
+        최대기간: 12,
+      },
+      {
+        은행명: "하나",
+        은행코드: "081",
+        적금명: "하나 장병내일준비적금",
+        기본금리: 4.0,
+        최대금리: 5.5,
+        최소기간: 6,
+        최대기간: 12,
+      },
+      {
+        은행명: "NH",
+        은행코드: "011",
+        적금명: "NH 장병내일준비적금",
+        기본금리: 4.5,
+        최대금리: 5,
+        최소기간: 6,
+        최대기간: 12,
+      },
+    ]);
+  }
   return (
     <SavingList>
-      {availableSavingList.map((item) => {
+      {sortedSavingList.map((item) => {
         return <AvailableSavingItem item={item} key={uuid()} />;
       })}
     </SavingList>
