0c186d68e17bb600d75e94b331df1a09a1dd622d
sieun <sieun0115@ajou.ac.kr>
2022-01-20 19:55:54 +0900

    #6 feat: 목표 이름, 목표 만기일 설정 컴포넌트 추가

---
+++ client/src/components/gather/DatePick.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { ko } from "date-fns/esm/locale";
+import { subDays } from "date-fns";
+import styled from "styled-components";
+
+const Container = styled.div`
+  .react-datepicker {
+    font-family: "Pretendard-regular";
+    button {
+      span::before {
+        border-color: var(--a1);
+      }
+    }
+    &__input-container {
+      input {
+        width: 100%;
+        box-sizing: border-box;
+        background: #fff;
+        border-radius: 8px;
+        border: none;
+        padding: 10px 16px;
+        font-family: "Pretendard-Regular";
+        font-size: 16px;
+        line-height: 25px;
+
+        &::placeholder {
+          font-size: 16px;
+          line-height: 25px;
+          color: #c5c5c5;
+        }
+      }
+    }
+
+    &__header {
+      background-color: #4caf5b26;
+      border-bottom: none;
+      padding: 0;
+      .react-datepicker__current-month {
+        padding: 10px 0;
+      }
+      .react-datepicker__day-names {
+        background-color: #fff;
+      }
+      .react-datepicker__day-name {
+        font-weight: 700;
+        color: var(--a4);
+      }
+    }
+    .react-datepicker__month {
+      margin-top: 0;
+    }
+  }
+`;
+
+function DeadLine() {
+  const [startDate, setStartDate] = useState(null);
+
+  return (
+    <Container>
+      <DatePicker
+        showPopperArrow={false}
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        minDate={subDays(new Date(), -32)}
+        placeholderText="목표한 날짜를 선택해주세요."
+        dateFormat="yyyy년 MM월 dd일"
+        dateFormatCalendar="yyyy년 MM월"
+        locale={ko}
+      />
+    </Container>
+  );
+}
+
+export default DeadLine;
--- client/src/components/gather/Goal.js
+++ client/src/components/gather/Goal.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled, { css } from "styled-components";
 import { styleTitle, styleSubTitle, styleNotice } from "style/common";
 import Category from "./Category";
+import DatePick from "components/gather/DatePick";
 
 const Container = styled.div`
   width: 100%;
@@ -9,16 +10,46 @@ const Container = styled.div`
   padding: 12px 20px 0;
   box-sizing: border-box;
   text-align: left;
+  position: relative;
 
   .Title {
     ${styleTitle}
   }
   .SubTitle {
     ${styleSubTitle}
+    padding: 0 0 4px 4px;
   }
   .Notice {
     ${styleNotice}
+    padding: 8px 0 0 4px;
   }
+  .Empasis {
+    ${styleNotice}
+    color: var(--Title_02);
+  }
+`;
+
+const GoalName = styled.div`
+  margin-top: 24px;
+  input {
+    width: 100%;
+    box-sizing: border-box;
+    background: #fff;
+    border-radius: 8px;
+    border: none;
+    padding: 10px 16px;
+    font-family: "Pretendard-Regular";
+    font-size: 16px;
+    line-height: 25px;
+
+    &::placeholder {
+      color: #c5c5c5;
+    }
+  }
+`;
+
+const DeadLine = styled.div`
+  margin-top: 24px;
 `;
 
 function Goal() {
@@ -31,6 +62,18 @@ function Goal() {
         다른 장병들은 <span>평균 {avgCnt}개</span>의 목표를 세웠어요.
       </div>
       <Category />
+      <GoalName>
+        <div className="SubTitle">목표 이름</div>
+        <input placeholder="목표 이름을 입력해주세요" />
+        <div className="Notice">예) 폰바꾸기</div>
+      </GoalName>
+      <DeadLine>
+        <div className="SubTitle">언제까지</div>
+        <DatePick />
+        <div className="Notice">
+          <spa className="Empasis">1</spa> 개월 후의 날짜부터 선택이 가능합니다.{" "}
+        </div>
+      </DeadLine>
     </Container>
   );
 }
