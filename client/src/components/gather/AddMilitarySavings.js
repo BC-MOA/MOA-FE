9f1f5a09d4a20ab1141c61e32b1c21ce769891f9
0andme <yu00mi97@gmail.com>
2022-01-20 15:51:24 +0900

    #5 feat : 군적금 사용자 안내 메시지 표시 기능 추가
    사용자 안내 메시지 표시 기능 추가

--- client/src/components/gather/AddMilitarySavings.js
+++ client/src/components/gather/AddMilitarySavings.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { backButton } from "style/common";
+import { backButtonBox } from "style/common";
 import styled from "styled-components";
 
 function AddMilitarySavings() {
   const history = useNavigate();
+  // todo - api datas
+  const userName = "민수";
+  const avgApplyNum = 1234;
 
   return (
     <Container>
@@ -18,6 +21,19 @@ function AddMilitarySavings() {
           alt="뒤로가기"
         />
       </BackButton>
+      <MessageBox>
+        <div className="title">
+          <span>{userName}님이 가입 가능한</span>
+          <span>군적금 상품들이에요.</span>
+        </div>
+        <div className="notice">
+          <span>
+            이번달 새로 군적금을 신청한 장병은{" "}
+            <span className="noticeBold">{avgApplyNum.toLocaleString()}</span>{" "}
+            명 입니다.
+          </span>
+        </div>
+      </MessageBox>
     </Container>
   );
 }
@@ -25,8 +41,35 @@ const Container = styled.div`
   margin: 8px 20px;
 `;
 const BackButton = styled.div`
-  ${backButton}
+  ${backButtonBox}
   margin-bottom:12px;
+`;
+const MessageBox = styled.div`
+  .title {
+    font-family: "Pretendard-SemiBold";
+    font-size: 21px;
+    line-height: 33px;
+    color: var(--Title_01);
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .notice {
+    font-family: "Pretendard-Regular";
+    font-size: 14px;
+    line-height: 22px;
+    color: var(--Body_01);
+
+    display: flex;
+    align-items: flex-start;
+  }
+  .noticeBold {
+    font-family: "Pretendard-Medium";
+    font-size: 16px;
+    line-height: 25px;
+    color: var(--Title_02);
+  }
 `;
 
 export default AddMilitarySavings;
