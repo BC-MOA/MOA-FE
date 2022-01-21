46e462696d1e31ae7fb0556d57139843c3d525b3
0andme <yu00mi97@gmail.com>
2022-01-21 19:31:46 +0900

    #5 feat : 군적금 약관 페이지 - 뒤로가기 헤더 추가

--- client/src/components/gather/AddMilitarySavingsTerm.js
+++ client/src/components/gather/AddMilitarySavingsTerm.js
@@ -1,7 +1,31 @@
+import BackHeader from "components/common/BackHeader";
 import React from "react";
+import { useLocation } from "react-router-dom";
+import { hideScrollBar } from "style/common";
+import styled from "styled-components";
 
 function AddMilitarySavingsTerm() {
-  return <div></div>;
-}
+  const { state: item } = useLocation();
 
+  return (
+    <Container>
+      <BackHeader path={-1} title={item.적금명} isScrolled={true}></BackHeader>
+      <ScrollBox></ScrollBox>
+    </Container>
+  );
+}
+const Container = styled.div`
+  padding: 8px 20px;
+  box-sizing: border-box;
+  background: var(--Surface);
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+const ScrollBox = styled.div`
+  ${hideScrollBar}
+  height: 100%;
+  padding-top: 12px;
+  box-sizing: border-box;
+`;
 export default AddMilitarySavingsTerm;
