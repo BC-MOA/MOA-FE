929c031260065c5481ce14f3aaf055ad75729a76
0andme <yu00mi97@gmail.com>
2022-01-22 04:37:18 +0900

    #5 feat : 군적금 약관동의 페이지 - 페이지번호 타이틀 공지사항 표시 기능 추가

--- client/src/components/gather/AddMilitarySavingsTerm.js
+++ client/src/components/gather/AddMilitarySavingsTerm.js
@@ -10,7 +10,19 @@ function AddMilitarySavingsTerm() {
   return (
     <Container>
       <BackHeader path={-1} title={item.적금명} isScrolled={true}></BackHeader>
-      <ScrollBox></ScrollBox>
+      <ScrollBox>
+        <TermPageNum>
+          <span className="bold">1</span>
+          <span>/</span>
+          <span>2</span>
+        </TermPageNum>
+        <TermPageTitle>약관동의</TermPageTitle>
+        <TermPageNotice>
+          <span>적금계좌 개설을 위한 </span>
+          <span className="highLight">약관 및 동의서</span>
+          <span>를 확인합니다.</span>
+        </TermPageNotice>
+      </ScrollBox>
     </Container>
   );
 }
@@ -25,7 +37,45 @@ const Container = styled.div`
 const ScrollBox = styled.div`
   ${hideScrollBar}
   height: 100%;
-  padding-top: 12px;
+  padding-top: 16px;
   box-sizing: border-box;
 `;
+const TermPageNum = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 4px;
+  font-family: "Roboto";
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 25px;
+  color: var(--Body_02);
+  margin-bottom: 4px;
+  .bold {
+    font-weight: 500;
+    color: var(--Title_02);
+  }
+`;
+const TermPageTitle = styled.div`
+  font-family: "Pretendard-SemiBold";
+  font-weight: 600;
+  font-size: 21px;
+  line-height: 33px;
+  color: var(--Title_01);
+  text-align: start;
+  margin-bottom: 4px;
+`;
+const TermPageNotice = styled.div`
+  font-family: "Pretendard-Regular";
+  font-size: 14px;
+  line-height: 22px;
+  color: var(--Body_01);
+  text-align: start;
+  margin-bottom: 28px;
+
+  .highLight {
+    color: var(--a3);
+  }
+`;
+
 export default AddMilitarySavingsTerm;
