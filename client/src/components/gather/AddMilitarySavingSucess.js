c200296cafde5f79923e0ff10420864dccf03c8d
0andme <yu00mi97@gmail.com>
2022-01-23 00:37:28 +0900

    #5 feat : 군적금 가입 현황 표시 및 작성 폼 데이터 표시 기능 추가

--- client/src/components/gather/AddMilitarySavingSucess.js
+++ client/src/components/gather/AddMilitarySavingSucess.js
@@ -4,6 +4,8 @@ import SubmitButton from "components/common/SubmitButton";
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import ApplyDataCard from "./ApplyDataCard";
+import ProcessStateBox from "./ProcessStateBox";
 
 function AddMilitarySavingSuccess() {
   const history = useNavigate();
@@ -19,39 +21,14 @@ function AddMilitarySavingSuccess() {
           />
         </SuccessIcon>
         <Notice>군적금을 신청했어요</Notice>
-        <ProcessStateBox>
-          <div className="currentState">
-            <img
-              src={require("assets/gather/ic_process_apply.svg").default}
-              alt="신청접수"
-            />
-            <span>신청 접수</span>
-          </div>
-          <div>
-            <img
-              src={require("assets/gather/ic_process_id_check.svg").default}
-              alt="군정보확인"
-            />
-            <span>군 정보 확인</span>
-          </div>
-
-          <div>
-            <img
-              src={require("assets/gather/ic_process_evaluation.svg").default}
-              alt="은행심사"
-            />
-            <span>은행 심사</span>
-          </div>
-          <div>
-            <img
-              src={
-                require("assets/gather/ic_process_maked_account.svg").default
-              }
-              alt="개설완료"
-            />
-            <div>개설 완료</div>
-          </div>
-        </ProcessStateBox>
+        <ProcessStateBox></ProcessStateBox>
+        <CharacterImg>
+          <img
+            src={require("assets/gather/army_character.svg").default}
+            alt="군인캐릭터"
+          />
+        </CharacterImg>
+        <ApplyDataCard formData={formData}></ApplyDataCard>
       </ScrollBox>
       <SubmitButton
         title={"확인"}
@@ -73,28 +50,11 @@ const Notice = styled.div`
   line-height: 38px;
   margin-bottom: 24px;
 `;
-const ProcessStateBox = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 20px;
-  font-family: "Pretendard-Regular";
-  font-size: 14px;
-  line-height: 22px;
-  color: var(--Body_01);
-  margin-bottom: 24px;
-  div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 4px;
-    img {
-      width: 49px;
-    }
-    &.currentState {
-      color: var(--a2);
-    }
-  }
+
+const CharacterImg = styled.div`
+  height: 64px;
+  overflow: hidden;
+  text-align: end;
 `;
 
 export default AddMilitarySavingSuccess;
---
+++ client/src/components/gather/ApplyDataCard.js
@@ -0,0 +1,70 @@
+import React, { Fragment } from "react";
+import styled from "styled-components";
+
+function ApplyDataCard({ formData }) {
+  return (
+    <ApplyDataCardStyle>
+      <div className="cardItem">
+        <span>군적금 이름</span>
+        <span className="bold">적금</span>
+      </div>
+      <div className="cardItem">
+        <span>기간</span>
+        <div>
+          <span className="bold">12</span>
+          <span className="bold roboto">개월</span>
+        </div>
+      </div>
+      {/* 자유입금일떄는? */}
+      <div className="cardItem">
+        <span>자동이체</span>
+        <div>
+          <span className="bold">매월 </span>
+          <span className="bold roboto">10</span>
+          <span className="bold">일</span>
+        </div>
+      </div>
+      <div className="cardItem">
+        <span>금액</span>
+        <div>
+          <span className="bold green roboto">2000</span>
+          <span></span> <span className="bold green">원</span>
+        </div>
+      </div>
+      <div className="cardItem">
+        <span>출금계좌</span>
+        <span className="bold">통장</span>
+      </div>
+    </ApplyDataCardStyle>
+  );
+}
+const ApplyDataCardStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 20px 24px;
+  background: #ffffff;
+  box-shadow: 0px 1px 2px rgba(33, 33, 33, 0.08);
+  border-radius: 12px;
+  font-family: "Pretendard-Regular";
+  font-size: 14px;
+  line-height: 22px;
+  color: var(--Body_01);
+  .cardItem {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    .bold {
+      font-family: "Pretendard-Medium";
+      color: var(--Title_02);
+      &.roboto {
+        font-family: "Roboto";
+        font-weight: 500;
+      }
+      &.green {
+        color: var(--a2);
+      }
+    }
+  }
+`;
+export default ApplyDataCard;
