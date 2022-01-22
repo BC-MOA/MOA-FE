af035f2a246fa668b4715b49c52d1d53e9d4025c
0andme <yu00mi97@gmail.com>
2022-01-22 19:17:14 +0900

    #5 feat : 폼 제출 버튼 추가

--- client/src/components/gather/AddMilitarySavingsForm.js
+++ client/src/components/gather/AddMilitarySavingsForm.js
@@ -1,15 +1,27 @@
 import BackHeader from "components/common/BackHeader";
 import Container from "components/common/Container";
 import ScrollBox from "components/common/ScrollBox";
+import SubmitButton from "components/common/SubmitButton";
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react/cjs/react.development";
 
 function AddMilitarySavingsForm() {
   const { state: item } = useLocation();
+  const history = useNavigate();
+  const [isAvildForm, setIsAvildForm] = useState(false);
+  const [formData, setFormData] = useState({});
   return (
     <Container>
       <BackHeader path={-1} title={item.적금명} isScrolled={true}></BackHeader>
       <ScrollBox paddingValue={"16px 0 40px "}></ScrollBox>
+      <SubmitButton
+        title={"신청 완료"}
+        onClickFunc={() => {
+          history("success", { state: formData });
+        }}
+        isActive={isAvildForm}
+      ></SubmitButton>
     </Container>
   );
 }
