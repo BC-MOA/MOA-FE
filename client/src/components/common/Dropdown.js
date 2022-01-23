1c83ea743da50d151cb3b54565b3f04e592147ac
0andme <yu00mi97@gmail.com>
2022-01-24 07:00:55 +0900

    #5 feat : 드롭다운 컴포넌트 추가

---
+++ client/src/components/common/Dropdown.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { v1 as uuid } from "uuid";
+import styled from "styled-components";
+
+function Dropdown({ options, selectValue, setValue, placeHolder }) {
+  const bgUrl = require("assets/gather/ic_select_arrow.svg").default;
+  return (
+    <DropdownBox
+      onChange={(e) => {
+        setValue(e.target.value);
+      }}
+      className={selectValue !== "" ? "isSelect" : ""}
+      bgUrl={bgUrl}
+      name="Dropdown"
+      value={selectValue}
+    >
+      <option value="" disabled hidden>
+        {placeHolder}
+      </option>
+      {options.map((item) => (
+        <option value={item} key={uuid()}>
+          {item}
+        </option>
+      ))}
+    </DropdownBox>
+  );
+}
+
+const DropdownBox = styled.select`
+  width: 100%;
+  padding: 10px 16px;
+  border-radius: 8px;
+  border: none;
+  font-family: "Pretendard-Regular";
+  font-size: 16px;
+  line-height: 25px;
+  color: var(--Line_01);
+  background: ${(props) =>
+    `url(${props.bgUrl})no-repeat #fff 97% 50%/16px auto;`};
+  -webkit-appearance: none; /* for chrome */
+  -moz-appearance: none; /*for firefox*/
+  ::-ms-expand {
+    display: none; /*for IE10,11*/
+  }
+  :hover {
+    cursor: pointer;
+  }
+  &.isSelect {
+    color: var(--Title_02);
+  }
+`;
+export default Dropdown;
