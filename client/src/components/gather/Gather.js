d2018283f76dccacbe2c6d03190f4c18be6823cd
0andme <yu00mi97@gmail.com>
2022-01-24 18:31:15 +0900

    #5 feat : 모으기 페이지 - 군적금 신청 버튼 추가

--- client/src/components/gather/Gather.js
+++ client/src/components/gather/Gather.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Gather() {
-  return <div>Gather</div>;
+  const history = useNavigate();
+
+  return (
+    <div>
+      <span>gater</span>
+      <button
+        onClick={() => {
+          history("/gather/add-militarySaving");
+        }}
+      >
+        군적금 신청
+      </button>
+    </div>
+  );
 }
 
 export default Gather;
