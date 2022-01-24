d927fe32ca9b7581aa451ff45c64311707937dc2
0andme <yu00mi97@gmail.com>
2022-01-24 21:02:45 +0900

    #17 feat : 하단 네비게이션 컴포넌트 추가

---
+++ client/src/components/common/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+function NavBar() {
+  return <NavBarBox></NavBarBox>;
+}
+const NavBarBox = styled.div``;
+export default NavBar;
