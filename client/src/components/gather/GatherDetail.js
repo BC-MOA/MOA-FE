c60ece085710dd1ef0f8c4f179a16f545a43e346
sieun <sieun0115@ajou.ac.kr>
2022-01-26 22:11:40 +0900

    #25 feat: 모으기 상세 페이지 추가

--- client/src/components/gather/GatherDetail.js
+++ client/src/components/gather/GatherDetail.js
@@ -1,26 +1,81 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import BackHeader from "components/common/BackHeader";
-import Tag from "components/common/Tag";
-import CustomBtn from "./addGoal/CustomBtn";
+import DetailCard from "./DetailCard";
+import TransactionEl from "./TransactionEl";
+import { hideScrollBar } from "style/common";
 
-const Container = styled.div``;
-const Content = styled.div`
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  background-color: #fff;
-  padding: 24px 20px;
-  box-shadow: 0px 1px 2px rgba(33, 33, 33, 0.08);
-  border-radius: 12px;
+  .title {
+    font-size: 18px;
+    line-height: 28px;
+    color: var(--Title_01);
+    font-family: "Pretendard-SemiBold";
+    text-align: left;
+    margin-bottom: 16px;
+  }
+  .number {
+    font-family: "Roboto";
+  }
 `;
+
+const Content = styled.div`
+  ${hideScrollBar}
+  flex: 1;
+
+  &.zero {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+const TransHistory = styled.div`
+  ${({ category }) =>
+    category === "군적금" &&
+    css`
+      path.main {
+        fill: #4caf5b;
+      }
+    `}
+  ${({ category }) =>
+    category === "목표" &&
+    css`
+      path.main {
+        fill: var(--Blue);
+      }
+    `}
+  ${({ category }) =>
+    category === "비상금" &&
+    css`
+      path.main {
+        fill: #f8cb57;
+      }
+    `}
+`;
+const TransZero = styled.div`
+  img {
+    filter: grayscale();
+  }
+  .zeroText {
+    margin-top: 12px;
+    font-family: "Pretendard-Regular";
+    color: var(--Body_01);
+    font-size: 16px;
+    line-height: 25px;
+  }
+`;
+
 function GatherDetail() {
   const userInfo = {
     startDate: "2020-01-10",
     endDate: "2022.12.10",
   };
   const gatherInfo = {
-    category: "군적금",
+    category: "비상금",
     name: "320만원 모으기",
     currentAmount: 800000,
     targetAmount: 3200000,
@@ -29,23 +84,36 @@ function GatherDetail() {
       name: "신한",
       number: "112-0330-0201",
     },
+    sDate: "Mon Jan 24 2022 15:11:39 GMT+0900",
+    eDate: "Sat Jan 29 2022 23:59:59 GMT+0900",
   };
+
+  const tr_lists = [];
   return (
     <Container>
       <BackHeader path={-1} title={gatherInfo.name} isScrolled={true} />
-      <Content>
-        <Tag>군적금</Tag>
-        <div>
-          <span>{gatherInfo.currentAmount} 원</span>
-        </div>
-        <div>
-          {gatherInfo.account.name} {gatherInfo.account.number}
-        </div>
-        <div>
-          {" "}
-          목표금액 <span>{gatherInfo.targetAmount}</span> 원
-        </div>
-        <CustomBtn>입금하기</CustomBtn>
+      <DetailCard gatherInfo={gatherInfo} />
+      <div className="title">거래내역</div>
+      <Content className={tr_lists.length ? "" : "zero"}>
+        {tr_lists.length ? (
+          <>
+            <TransHistory category={gatherInfo.category}>
+              {tr_lists.map((x, idx) => (
+                <TransactionEl key={idx} lists={x.lists}>
+                  {x.date}
+                </TransactionEl>
+              ))}
+            </TransHistory>
+          </>
+        ) : (
+          <TransZero>
+            <img
+              src={require("assets/gather/character_head.svg").default}
+              alt="캐릭터 얼굴"
+            />
+            <div className="zeroText">아직 거래내역이 없어요</div>
+          </TransZero>
+        )}
       </Content>
     </Container>
   );
