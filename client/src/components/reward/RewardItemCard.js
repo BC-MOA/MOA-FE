caffe212204647e7e4badd670d630405f14f9919
0andme <yu00mi97@gmail.com>
2022-01-26 20:07:24 +0900

    #34 feat : 리워드 페이지 - 리워드 상품 카드 컴포넌트화
    상자 오픈 시 나오는 리워드 선택 카드 컴포넌트화

---
+++ client/src/components/reward/RewardItemCard.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+function RewardItemCard({ itemName, selectedItem, setSelectedItem }) {
+  return (
+    <ItemCard
+      onClick={() => {
+        setSelectedItem(itemName);
+      }}
+      className={itemName === selectedItem ? "isSelect" : ""}
+    >
+      <img
+        src={require("assets/reward/reward_item_coffee.png")}
+        alt={itemName}
+      />
+      <span>{itemName}</span>
+      <button className={itemName === selectedItem ? "isSelect" : ""}>
+        {itemName === selectedItem ? "선택됨" : "선택"}
+      </button>
+    </ItemCard>
+  );
+}
+const ItemCard = styled.div`
+  width: 160px;
+  border-radius: 10px;
+  padding: 16px;
+  box-sizing: border-box;
+  background-color: var(--Line_03);
+  font-family: "Pretendard-SemiBold";
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 22px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  img {
+    width: 70px;
+    margin-bottom: 6px;
+  }
+  span {
+    margin-bottom: 8px;
+  }
+  button {
+    line-height: 22px;
+    padding: 3px 24px;
+    background-color: var(--Body_01);
+    border-radius: 10px;
+    color: #fff;
+    border: none;
+  }
+  &.isSelect {
+    background-color: rgba(76, 175, 91, 0.15);
+    button {
+      background-color: var(--a2);
+    }
+  }
+`;
+export default RewardItemCard;
