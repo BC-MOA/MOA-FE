e33c7aea112e5bbee0a006bc5dfed1616ba1a4fc
sieun <sieun0115@ajou.ac.kr>
2022-01-28 03:43:19 +0900

    #25 refact: 컴포넌트 재사용을 위한 코드 수정

--- client/src/components/gather/addGoal/Category.js
+++ client/src/components/gather/addGoal/Category.js
@@ -11,41 +11,7 @@ const CategoryBox = styled.div`
   border-radius: 8px;
 `;
 
-function Category() {
-  // const categories = [
-  //   {
-  //     en_Name: "travel",
-  //     kr_Name: "여행",
-  //   },
-  //   {
-  //     en_Name: "electronic",
-  //     kr_Name: "전자기기",
-  //   },
-  //   {
-  //     en_Name: "gift",
-  //     kr_Name: "선물",
-  //   },
-  //   {
-  //     en_Name: "date",
-  //     kr_Name: "데이트",
-  //   },
-  //   {
-  //     en_Name: "clothes",
-  //     kr_Name: "의류",
-  //   },
-  //   {
-  //     en_Name: "vacation",
-  //     kr_Name: "휴가비",
-  //   },
-  //   {
-  //     en_Name: "medical",
-  //     kr_Name: "의료비",
-  //   },
-  //   {
-  //     en_Name: "etc",
-  //     kr_Name: "기타",
-  //   },
-  // ];
+function Category({ inputs, setInputs }) {
   const categories = [
     "여행",
     "전자기기",
@@ -59,10 +25,9 @@ function Category() {
 
   return (
     <>
-      <div className="SubTitle">무엇을 위해서?</div>
       <CategoryBox>
         {categories.map((El, idx) => (
-          <CategoryEl El={El} key={idx} />
+          <CategoryEl El={El} key={idx} inputs={inputs} setInputs={setInputs} />
         ))}
       </CategoryBox>
     </>
