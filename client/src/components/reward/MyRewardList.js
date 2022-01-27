6f60e8b8bc3453a2387763e3cd3ac1bd0a48fc04
0andme <yu00mi97@gmail.com>
2022-01-28 02:51:35 +0900

    #39 feat : 리워드 - 보관함 페이지 - 리워드 탭 - 상품 리스트 컴포넌트 추가

--- client/src/components/reward/MyRewardList.js
+++ client/src/components/reward/MyRewardList.js
@@ -1,10 +1,25 @@
 import ContentControlBtn from "components/common/ContentControlBtn";
 import ScrollBox from "components/common/ScrollBox";
+import MyRewardListItem from "components/reward/MyRewardListItem";
 import React, { useState } from "react";
 import styled from "styled-components";
+import { v1 as uuid } from "uuid";
 const controlNameList = ["부대 내", "부대 외"];
 function MyRewardList({ tabName, tabList }) {
   const [listControl, setListControl] = useState(controlNameList[0]);
+  const [inReward, setInReward] = useState([
+    { name: "불닭볶음면" },
+    { name: "바나나우유" },
+  ]);
+  const [outReward, setOutReward] = useState([
+    { name: "불닭볶음면" },
+    { name: "포스틱" },
+    { name: "커피" },
+    { name: "짜파게티" },
+    { name: "아이스크림" },
+  ]);
+
+  // todo - 사용자 리워드 내역을 받아와 부대내외로 값을 나눠 저장
   return (
     <MyRewardListStyle className={tabList[1] === tabName ? "isSelect" : ""}>
       <ContentControlBtn
@@ -13,14 +28,40 @@ function MyRewardList({ tabName, tabList }) {
         listControl={listControl}
         setListControl={setListControl}
         controlNameList={controlNameList}
-      ></ContentControlBtn>
-      <ScrollBox paddingValue={"20px 0"}></ScrollBox>
+      />
+      <ScrollBox paddingValue={"20px 0"}>
+        <div className="itemList">
+          {controlNameList[0] === listControl &&
+            inReward &&
+            inReward.map((item) => (
+              <MyRewardListItem
+                key={uuid()}
+                item={item}
+                listControl={listControl}
+              ></MyRewardListItem>
+            ))}
+        </div>
+        <div className="itemList">
+          {controlNameList[1] === listControl &&
+            outReward &&
+            outReward.map((item) => (
+              <MyRewardListItem
+                key={uuid()}
+                item={item}
+                listControl={listControl}
+              ></MyRewardListItem>
+            ))}
+        </div>
+      </ScrollBox>
     </MyRewardListStyle>
   );
 }
 const MyRewardListStyle = styled.div`
   display: none;
   &.isSelect {
+    display: block;
+  }
+  .itemList {
     display: flex;
     gap: 15px;
     flex-wrap: wrap;
