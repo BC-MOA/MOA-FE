8b4bd410657b76f148f1d675b71fb891a85fa4e9
0andme <yu00mi97@gmail.com>
2022-01-28 18:28:44 +0900

    #39 feat : 바코드 수정 - 교환권 이미지 저장 기능 - 교환권 정보 표시

--- client/src/components/reward/MyProducItemDetail.js
+++ client/src/components/reward/MyProducItemDetail.js
@@ -1,17 +1,39 @@
 import Container from "components/common/Container";
-import React from "react";
+import SubmitButton from "components/common/SubmitButton";
+import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-const BARCODE_URL = process.env.REACT_APP_BARCODE;
+import html2canvas from "html2canvas";
+import JsBarcode from "jsbarcode";
 
 function MyProducItemDetail() {
   const { state } = useLocation();
   const history = useNavigate();
   const item = state.item;
-  const barCodeNum = 1234567890;
+  const barCodeNum = "1234 5678 9012";
   function copy() {
     navigator.clipboard.writeText(barCodeNum);
   }
+  // todo - 사진 저장 파일명 포맷 변경
+  function SaveProductImg() {
+    html2canvas(document.getElementById("exportImgBox")).then((canvas) => {
+      const imgUrl = canvas.toDataURL("image/jpg");
+      const imgFileName = "image.jpg";
+      const link = document.createElement("a");
+      console.log(link);
+      document.body.appendChild(link);
+      link.href = imgUrl;
+      link.download = imgFileName;
+      link.click();
+      document.body.removeChild(link);
+    });
+  }
+  useEffect(() => {
+    JsBarcode("#barcode", barCodeNum, {
+      displayValue: false,
+      background: "transparent",
+    });
+  }, []);
   return (
     <Container>
       <Header>
@@ -24,32 +46,42 @@ function MyProducItemDetail() {
         />
         {/* todo item의 이미지로 변경할 것 */}
       </Header>
-      <ImgCard>
-        <img src={require("./example.png")} alt={item.name} />
-        <div>px 간식</div>
-        <div className="itemName">{item.name}</div>
-      </ImgCard>
-      {/* todo 바코드 text변경하기*/}
-      <BarCode>
-        <img
-          src={`${BARCODE_URL}?bcid=code128&text=${barCodeNum}`}
-          alt="barcode"
-        />
-        <div className="barcodeDetail">
-          <p>{barCodeNum}</p>
-          <button onClick={copy}>번호복사</button>
-        </div>
-      </BarCode>
-      {/* 교환처 */}
-      {/*  유효기간*/}
-      {/* 버튼 */}
+      <div id="exportImgBox">
+        <ImgCard>
+          <img src={require("./example.png")} alt={item.name} />
+          <div>px 간식</div>
+          <div className="itemName">{item.name}</div>
+        </ImgCard>
+        {/* todo 바코드 text변경하기*/}
+        <BarCode>
+          <img id="barcode" alt="barcode" />
+          <div className="barcodeDetail">
+            <p>{barCodeNum}</p>
+            <button onClick={copy}>번호복사</button>
+          </div>
+        </BarCode>
+        <Detail>
+          <span>교환처</span>
+          <span className="bold">부대 안 PX</span>
+        </Detail>
+        <Detail className="last">
+          <span>유효기간</span>
+          <span className="bold">2022. 05. 16</span>
+        </Detail>
+      </div>
+      <SubmitButton
+        title={"교환권 저장"}
+        onClickFunc={() => {
+          SaveProductImg();
+        }}
+        isActive={true}
+      />
     </Container>
   );
 }
 const Header = styled.div`
   z-index: 100;
   padding: 5px 4px;
-  margin-bottom: calc(28px - 5px);
   text-align: end;
   img {
     width: 18px;
@@ -67,7 +99,7 @@ const ImgCard = styled.div`
   font-size: 14px;
   line-height: 22px;
   color: var(--Body_01);
-  margin: 0 auto 30px;
+  margin: 23px auto 30px;
   padding: 28px 40px;
   box-sizing: border-box;
   img {
@@ -82,7 +114,7 @@ const ImgCard = styled.div`
   }
 `;
 const BarCode = styled.div`
-  margin: 0 auto;
+  margin: 0 auto 53px;
   img {
     width: 232px;
     height: 100px;
@@ -92,12 +124,15 @@ const BarCode = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    width: 236px;
+    margin: 0 auto;
     p {
       font-family: "Roboto";
       font-weight: 700;
       font-size: 21px;
       line-height: 33px;
       color: var(--Title_01);
+      margin-right: 8px;
     }
     button {
       border: none;
@@ -109,6 +144,24 @@ const BarCode = styled.div`
       font-size: 14px;
       line-height: 22px;
     }
+  }
+`;
+const Detail = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-family: "Pretendard-Medium";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 25px;
+  color: var(--Body_01);
+  margin: 0 30px 12px;
+  &.last {
+    margin: 0 30px 40px;
+  }
+  .bold {
+    color: var(--Title_01);
   }
 `;
 export default MyProducItemDetail;
