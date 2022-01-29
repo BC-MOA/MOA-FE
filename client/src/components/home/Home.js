32579585d8d50364ae885706c34b9c13366276f7
0andme <yu00mi97@gmail.com>
2022-01-29 20:38:23 +0900

    #62 feat : 계좌/군적금 연동 여부와 무관한 컴포넌트 추가

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,40 +1,151 @@
-import axios from "axios";
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
+import Container from "components/common/Container";
+import { Header } from "components/common/Header";
+import NavBar from "components/common/NavBar";
+import ScrollBox from "components/common/ScrollBox";
+import React, { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { v1 as uuid } from "uuid";
 function Home() {
-  const history = useNavigate();
-  function test() {
-    axios
-      .get("/v1/hello")
-      .then((res) => {
-        console.log(res.data.json);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+  // const history = useNavigate();
+  const [challengeList, setChallengeList] = useState([]);
   useEffect(() => {
-    test();
+    // todo 인기 챌린지 받아오기
+    setChallengeList([
+      { name: "1", type1: "A", type2: "B", 참여자: "100" },
+      { name: "2", type1: "A", type2: "B", 참여자: "200" },
+      { name: "3", type1: "A", type2: "B", 참여자: "300" },
+    ]);
   }, []);
   return (
-    <div>
-      <span>home</span>
-      <button
-        onClick={() => {
-          history("/gather");
-        }}
-      >
-        모으기
-      </button>
-      <button
-        onClick={() => {
-          test();
-        }}
-      >
-        api test
-      </button>
-    </div>
+    <Container>
+      <Header title={true} keys={3000} alarm={false}></Header>
+      <ScrollBox paddingValue={"24px 0 "}>
+        {/* 군적금 계좌,적금 연결 안 했을 때 */}
+        <div>
+          <AboutMoa
+            src={require("assets/about_moa.png")}
+            alt={"모아이용방법확인하기"}
+          />
+        </div>
+        {/* 군적금 계좌,적금 연결 했을 때 */}
+        <div></div>
+        {/* 공통 */}
+        <AboutChallenge>
+          <div className="mainTitle">
+            <span>현재 진행중인 </span>
+            <span className="green">인기 챌린지</span>
+          </div>
+          <div className="challengeList">
+            {challengeList &&
+              challengeList.map((item) => (
+                <div className="challengeItem" key={uuid()}>
+                  <img src={require("./example.png")} alt="" />
+                  <div className="about">
+                    <div className="title">{item.name}</div>
+                    <div className="subtitle">
+                      <span>
+                        {item.type1} vs {item.type2}
+                      </span>
+                      <div className="tag">
+                        <span className="roboto">{item.참여자}</span>명 참여
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+          <button>더보기</button>
+        </AboutChallenge>
+        <img
+          src={require("assets/about_reward.png")}
+          alt={"모아박스구경가기"}
+        />
+      </ScrollBox>
+      <NavBar />
+    </Container>
   );
 }
+// style
+
+const AboutMoa = styled.img`
+  display: block;
+  margin-bottom: 54px;
+`;
+const AboutChallenge = styled.div`
+  margin-bottom: 40px;
+  .mainTitle {
+    font-family: "Pretendard-SemiBold";
+    font-size: 18px;
+    line-height: 28px;
+    text-align: start;
+    margin-bottom: 20px;
+
+    .green {
+      color: var(--a2);
+    }
+  }
+  .challengeList {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    margin-bottom: 12px;
+  }
+
+  .challengeItem {
+    display: flex;
+    align-items: center;
+    background: #ffffff;
+    box-shadow: 0px 1px 2px rgba(33, 33, 33, 0.08);
+    border-radius: 12px;
+    padding: 16px 20px;
+
+    .about {
+      margin-left: 12px;
+      text-align: start;
+      flex-grow: 1;
+      .title {
+        font-family: "Pretendard-SemiBold";
+        font-size: 16px;
+        line-height: 25px;
+        color: var(--Title_01);
+      }
+      .subtitle {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-family: "Pretendard-Medium";
+        color: var(--Body_01);
+        font-size: 14px;
+        line-height: 22px;
+        .tag {
+          display: flex;
+          align-items: center;
+          background: rgba(76, 175, 91, 0.15);
+          border-radius: 8px;
+          font-family: "Pretendard-Medium";
+          font-size: 12px;
+          color: var(--a2);
+          padding: 0 8px;
+          .roboto {
+            font-family: "Roboto";
+            font-weight: 500;
+            font-size: 13px;
+          }
+        }
+      }
+    }
+  }
+  button {
+    width: 100%;
+    padding: 12px 0;
+    border-radius: 12px;
+    font-family: "Pretendard-Medium";
+    font-size: 16px;
+    line-height: 25px;
+    border: none;
+    color: var(--Body_01);
+    background-color: var(--Line_03);
+  }
+`;
 export default Home;
