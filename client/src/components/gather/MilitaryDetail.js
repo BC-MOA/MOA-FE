74dcaf2f3be4a6c99161f2fd4ac00d25af130955
0andme <yu00mi97@gmail.com>
2022-02-03 18:21:19 +0900

    #69 feat : 군적금 신청 현황 페이지 뒤로가기 헤더 추가

--- client/src/components/gather/MilitaryDetail.js
+++ client/src/components/gather/MilitaryDetail.js
@@ -1,8 +1,20 @@
+import BackHeader from "components/common/BackHeader";
 import Container from "components/common/Container";
+import ScrollBox from "components/common/ScrollBox";
 import React from "react";
 
 function MilitaryDetail() {
-  return <Container></Container>;
+  return (
+    <Container>
+      {/* todo - title 변경하기 - 목표명 있을 때는 목표명 / 없을 때는 ''  */}
+      <BackHeader
+        path={"/gather"}
+        title={"1000만원 모으기"}
+        isScrolled={true}
+      ></BackHeader>
+      <ScrollBox></ScrollBox>
+    </Container>
+  );
 }
 
 export default MilitaryDetail;
