1b188078c0bed3d67c3e5368ce0130a255fb205d
sieun <sieun0115@ajou.ac.kr>
2022-02-03 19:03:16 +0900

    design: 챌린지 navBar 위치 수정

--- client/src/components/compete/Compete.js
+++ client/src/components/compete/Compete.js
@@ -9,13 +9,20 @@ import { allCompList, myCompList } from "./context/data";
 
 //임시 하단 nav
 const StyledNavBar = styled(NavBar)`
-  position: fixed;
-  bottom: 0;
+  /* position: fixed; */
+  /* bottom: 0; */
+`;
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
 `;
 
 function Compete() {
   return (
-    <>
+    <Container>
       <MyCompeteContext.Provider value={myCompList.compList}>
         <AllCompContext.Provider value={allCompList.compList}>
           <Header title={false} keys={3000} alarm={true}></Header>
@@ -23,7 +30,7 @@ function Compete() {
         </AllCompContext.Provider>
       </MyCompeteContext.Provider>
       <StyledNavBar></StyledNavBar>
-    </>
+    </Container>
   );
 }
 
