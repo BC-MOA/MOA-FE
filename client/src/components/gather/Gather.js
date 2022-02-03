fe31784a9b6213ce6d0fc9bfc283bb55a86c917b
sieun <sieun0115@ajou.ac.kr>
2022-02-03 13:05:51 +0900

    #25 refact: 모으기 더미 데이터 파일 분리 후, 정보 가져오기

--- client/src/components/gather/Gather.js
+++ client/src/components/gather/Gather.js
@@ -7,6 +7,7 @@ import NavBar from "components/common/NavBar";
 import StateGather from "components/gather/StateGather";
 import { ReactSortable } from "react-sortablejs";
 import moment from "moment";
+import { gatherList } from "components/common/dummyData";
 
 const Container = styled.div`
   width: 100%;
@@ -72,104 +73,7 @@ const EditBtn = styled.button`
 
 function Gather() {
   const userName = "민수";
-  const gatherList = [
-    {
-      category: "군적금",
-      name: "320만원 모으기",
-      currentAmount: 800000,
-      targetAmount: 3200000,
-      account: {
-        name: "KB국민",
-        number: "112-0330-0201",
-      },
-      sDate: "Sun Oct 10 2021 15:11:39 GMT+0900",
-      eDate: "Fri Mar 10 2023 23:59:59 GMT+0900",
-      depositMethod: "자동이체",
-      howOften: "매월 10일",
-      amount: "200000",
-    },
-    {
-      category: "목표",
-      goal_category: "여행",
-      name: "나도 여행한번 가보자 유럽 전역으로!",
-      currentAmount: 170000,
-      targetAmount: 1000000,
-      account: {
-        name: "하나은행",
-        number: "123-456-78-910111",
-      },
-      sDate: "Wed Nov 10 2021 15:11:39 GMT+0900",
-      eDate: "Sat Dec 10 2022 23:59:59 GMT+0900",
-      depositMethod: "자동이체",
-      howOften: "매월 10일",
-      amount: "50000",
-    },
-    {
-      category: "비상금",
-      name: "비상금",
-      currentAmount: 100000,
-      account: {
-        name: "하나은행",
-        number: "123-456-78-103556",
-      },
-    },
-    // {
-    //   category: "비상금",
-    //   name: "비상금",
-    //   currentAmount: 300000,
-    //   account: {
-    //     name: "NH국민",
-    //     number: "123-456-78-103556",
-    //   },
-    // },
-    // {
-    //   category: "군적금",
-    //   name: "100만원 모으기",
-    //   currentAmount: 100000,
-    //   targetAmount: 100000,
-    //   account: {
-    //     name: "IBK기업",
-    //     number: "112-0330-0201-55",
-    //   },
-    //   sDate: "Sun Oct 10 2021 15:11:39 GMT+0900",
-    //   eDate: "Sat Dec 10 2022 23:59:59 GMT+0900",
-    //   depositMethod: "자동이체",
-    // howOften: "매월 10일",
-    //   amount: "200000",
-    // },
-    {
-      category: "목표",
-      goal_category: "선물",
-      name: "조카 선물😎",
-      currentAmount: 150000,
-      targetAmount: 150000,
-      account: {
-        name: "NH농협",
-        number: "356-0915-7261-11",
-      },
-      sDate: "Wed Nov 10 2021 15:11:39 GMT+0900",
-      eDate: "Wed Jan 26 2022 23:59:59 GMT+0900",
-      depositMethod: "자유입금",
-      howOften: "",
-      amount: "50000",
-    },
-    {
-      category: "군적금",
-      name: "1000만원 모으기",
-      currentAmount: 0,
-      targetAmount: 2400000,
-      account: {
-        name: "신한",
-        number: "112-0650-0987",
-      },
-      sDate: "Mon Jan 10 2022 15:11:39 GMT+0900",
-      eDate: "Fri Mar 10 2023 23:59:59 GMT+0900",
-      depositMethod: "자유입금",
-      howOften: "",
-      amount: "",
-    },
-  ];
-
+  //  dummyData 에서 gatherlist 정보 가지고 오기!
   const inProgressList = gatherList.filter((x) => !moment().isAfter(x.eDate));
   const completedList = gatherList.filter((x) => moment().isAfter(x.eDate));
   const totalAmount = inProgressList.reduce((acc, cur) => {
@@ -245,18 +149,6 @@ function Gather() {
           </>
         )}
       </Content>
-      {/* <EditBtn
-        onClick={() => {
-          setEditToggle(!editToggle);
-        }}
-        className={editToggle ? "" : "Active"}
-      >
-        <img
-          src={require("assets/gather/Sort_arrow_light.svg").default}
-          alt="순서변경 아이콘"
-        />
-        {editToggle ? "순서 편집하기" : "편집완료"}
-      </EditBtn> */}
       <NavBar />
     </Container>
   );
