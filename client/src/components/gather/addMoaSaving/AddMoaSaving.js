ac9ce4d869f6afe8eb8762d62618cff22fae1cdb
0andme <yu00mi97@gmail.com>
2022-02-05 17:26:01 +0900

    #86 feat : 제휴 통장 개설 메인 페이지 뒤로가기 헤더 및 타이틀 추가

--- client/src/components/gather/addMoaSaving/AddMoaSaving.js
+++ client/src/components/gather/addMoaSaving/AddMoaSaving.js
@@ -1,7 +1,35 @@
+import BackHeader from "components/common/BackHeader";
+import Container from "components/common/Container";
 import React from "react";
+import styled from "styled-components";
 
 function AddMoaSaving() {
-  return <div>AddMoaSaving</div>;
+  return (
+    <Container>
+      <BackHeader path={-1} isScrolled={true} title={""} />
+      <Title>{"민수"}님,</Title>
+      <Title>첫 {"목표"}를 세우시는군요!</Title>
+      <SubTitle>
+        목표 금액과 비상금은 KEB 하나은행과 모아(MOA)가 함께 제휴해 제공하는
+        통장에 모아져요
+      </SubTitle>
+    </Container>
+  );
 }
-
+const Title = styled.div`
+  font-family: "Pretendard-SemiBold";
+  font-size: 21px;
+  line-height: 33px;
+  color: var(--Title_01);
+  text-align: start;
+`;
+const SubTitle = styled.div`
+  margin-top: 4px;
+  font-family: "Pretendard-Regular";
+  font-size: 14px;
+  line-height: 22px;
+  color: var(--Body_01);
+  text-align: start;
+  margin-bottom: 84px;
+`;
 export default AddMoaSaving;
