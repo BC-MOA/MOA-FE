eba7a1db390962f81bde773732e7b0fad1169ad6
0andme <yu00mi97@gmail.com>
2022-02-05 17:56:25 +0900

    #86 feat : 제휴 통장 개설 성공 페이지 추가

--- client/src/components/gather/addMoaSaving/AddMoaSavingSuccess.js
+++ client/src/components/gather/addMoaSaving/AddMoaSavingSuccess.js
@@ -1,7 +1,57 @@
+import Container from "components/common/Container";
+import ScrollBox from "components/common/ScrollBox";
+import SubmitButton from "components/common/SubmitButton";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import ApplyDataCard from "./ApplyDataCard";
 
 function AddMoaSavingSuccess() {
-  return <div>성공</div>;
+  const history = useNavigate();
+  return (
+    <Container>
+      <ScrollBox paddingValue={"80px 0 114px "}>
+        <SuccessIcon>
+          <img
+            className="animate__animated animate__flipInX "
+            src={require("assets/gather/ic_big_check.svg").default}
+            alt="성공"
+          />
+        </SuccessIcon>
+        <Notice>입출금통장이 개설됐어요</Notice>
+        <CharacterImg>
+          <img
+            src={require("assets/gather/army_character.svg").default}
+            alt="군인캐릭터"
+          />
+        </CharacterImg>
+        <ApplyDataCard></ApplyDataCard>
+      </ScrollBox>
+      <SubmitButton
+        title={"확인"}
+        onClickFunc={() => {
+          history("/gather");
+        }}
+        isActive={true}
+      ></SubmitButton>
+    </Container>
+  );
 }
+const SuccessIcon = styled.div`
+  margin-bottom: 10px;
+`;
+const Notice = styled.div`
+  font-family: "Pretendard-SemiBold";
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 38px;
+  margin-bottom: 24px;
+`;
+
+const CharacterImg = styled.div`
+  height: 64px;
+  overflow: hidden;
+  text-align: end;
+`;
 
 export default AddMoaSavingSuccess;
