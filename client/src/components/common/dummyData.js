7908765a6dea07dde67ead953508afd2bf8b76d0
sieun <sieun0115@ajou.ac.kr>
2022-02-07 00:20:33 +0900

    #102 feat: 알림 페이지 추가

--- client/src/components/common/dummyData.js
+++ client/src/components/common/dummyData.js
@@ -131,3 +131,55 @@ export const accountList = [
     balance: 100000,
   },
 ];
+
+export const noticeList = [
+  {
+    id: 8,
+    category: "penalty",
+    date: "2월 02일",
+    content:
+      "‘신한 장병내일준비적금' 중도해지 패널티로 열쇠 6개가 차감되었어요",
+  },
+  {
+    id: 7,
+    category: "reward",
+    date: "1월 30일",
+    content: "야금야금MOA박스를 구매했어요",
+  },
+  {
+    id: 6,
+    category: "challenge",
+    date: "1월 29일",
+    content: "‘프리미어리그 경기 승자는?’ 챌린지 내기에 성공했어요!",
+  },
+  {
+    id: 5,
+    category: "key",
+    date: "1월 28일",
+    content: "첫 비상금 개설로 열쇠 3개를 획득했어요",
+  },
+  {
+    id: 4,
+    category: "account",
+    date: "1월 28일",
+    content: "첫 비상금을 개설했어요",
+  },
+  {
+    id: 3,
+    category: "key",
+    date: "1월 28일",
+    content: "첫 목표 ‘뉴욕여행 준비' 개설로 열쇠 3개를 획득했어요",
+  },
+  {
+    id: 2,
+    category: "key",
+    date: "1월 28일",
+    content: "첫 목표 ‘뉴욕여행 준비'를 개설했어요",
+  },
+  {
+    id: 1,
+    category: "key",
+    date: "1월 27일",
+    content: "첫 목표 ‘뉴욕여행 준비'를 개설했어요",
+  },
+];
