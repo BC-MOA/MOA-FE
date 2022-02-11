159ee5eb689286688c85b447d369b60fc36b70ce
0andme <yu00mi97@gmail.com>
2022-02-11 19:07:30 +0900

    feat: 말풍선 메시지 로직 추가

--- client/src/components/home/BubbleContent.js
+++ client/src/components/home/BubbleContent.js
@@ -1,36 +1,43 @@
-import React, { useContext } from "react";
+import React, { Fragment, useContext } from "react";
 import { UserData } from "store/User";
 import styled from "styled-components";
 
-function BubbleContent({ savingNum }) {
+function BubbleContent({ gatherNumList }) {
   const { userData } = useContext(UserData);
+  const isMax =
+    2 === gatherNumList.army &&
+    (0 < gatherNumList.goal || 0 < gatherNumList.safeBox);
   return (
-    <SpeechBubble>
-      {!userData.id && (
-        <div className="bubbleText">
-          <span>다양한 저축 방법으로 재밌게 돈을 모아보아요</span>
-          <span>3가지 방법으로 저축을 시작할 수 있어요</span>
-        </div>
+    <SpeechBubble IsMax={isMax}>
+      {!isMax && (
+        <Fragment>
+          {!userData.id && (
+            <div className="bubbleText">
+              <span>다양한 저축 방법으로 재밌게 돈을 모아보아요</span>
+              <span>3가지 방법으로 저축을 시작할 수 있어요</span>
+            </div>
+          )}
+          {userData.id && 0 === gatherNumList.army && (
+            <div className="bubbleText">
+              <span>군적금이 없는 {"민수"}님을 위해 추천드려요</span>
+              <span>군적금 가입하고 최대금리와 열쇠 받아가세요!</span>
+            </div>
+          )}
+          {userData.id && 1 === gatherNumList.army && (
+            <div className="bubbleText">
+              <span>군적금을 1개 더 가입하거나</span>
+              <span>목표를 잡아 추가 저축하고 열쇠 받아가세요!</span>
+            </div>
+          )}
+          {userData.id && 2 === gatherNumList.army && (
+            <div className="bubbleText">
+              <span>군적금은 이미 꽉 채워서 들고 계시네요</span>
+              <span>목표를 잡아 추가 저축하고 열쇠 받아가세요!</span>
+            </div>
+          )}
+          <div className="tail"></div>
+        </Fragment>
       )}
-      {userData.id && 0 === savingNum && (
-        <div className="bubbleText">
-          <span>군적금이 없는 {"민수"}님을 위해 추천드려요</span>
-          <span>군적금 가입하고 최대금리와 열쇠 받아가세요!</span>
-        </div>
-      )}
-      {userData.i && 1 === savingNum && (
-        <div className="bubbleText">
-          <span>군적금을 1개 더 가입하거나</span>
-          <span>목표를 잡아 추가 저축하고 열쇠 받아가세요!</span>
-        </div>
-      )}
-      {userData.id && 2 === savingNum && (
-        <div className="bubbleText">
-          <span>군적금은 이미 꽉 채워서 들고 계시네요</span>
-          <span>목표를 잡아 추가 저축하고 열쇠 받아가세요!</span>
-        </div>
-      )}
-      <div className="tail"></div>
       <img
         className="character"
         src={require("assets/army_character.svg").default}
@@ -48,8 +55,20 @@ const SpeechBubble = styled.div`
   font-size: 12px;
   line-height: 19px;
   position: relative;
-  padding-top: 21px;
-
+  padding-top: ${(props) => {
+    if (props.IsMax) {
+      return "0";
+    } else {
+      return "21px";
+    }
+  }};
+  margin-top: ${(props) => {
+    if (props.IsMax) {
+      return "-55px";
+    } else {
+      return "0";
+    }
+  }};
   .bubbleImg {
     width: 226px;
     height: 60px;
